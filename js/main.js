d224ddefd01c3adbeb45b1af07b36310aa2fba48
Sean Holden <seanholden1@gmail.com>
2014-02-25 14:45:05 +0000

    refactored loops in code using JQuery .each function

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function initialize(){
 }
 
 function plotGraph(xaxisTitles, data){
-  // console.log(JSON.stringify(xaxisTitles));
+  console.log(data.length);
 
   // TODO: make this loop depending on how many lines are entered
   var chart = [
@@ -51,33 +51,25 @@ function enterData(callback){
   var data = [];
   var parsedData = [];
   var xaxisTitles = [];
-  
+
   // Split all CSV lines entered by user into array of arrays, called "data"
-  for(var i=0;i<lines.length;i++){
-    var line = lines[i];
-    data.push(line.split(','))
-  };
+  $.each(lines, function(i, line){
+    data.push(line.split(','));
+  }); // => data = [["title1"," title2","title3"],["100","200","300"]]
 
-  // Get xaxis labels from line 1 of the user input and put them into usable format
-  // named xaxisTitles. Pass this into callback function.
-  for(var i=0;i<data[0].length;i++){
-    var title = data[0][i];
-    xaxisTitles.push([i+1, title]);
-  }
 
-  // data: [["sean"," luke","joe"],["100","200","300"]]
-  // needs to be: [[1,100],[2,200],[3,300]]
+  // Parse the data into flot friendly format.
+  $.each(data, function(i, line){
+    $.each(line, function(n, item){
+      if(i==0){
+        xaxisTitles.push([n+1,item]);
+      }else{
+        parsedData.push([n+1,parseInt(item)]);
+      };
+    });
+  }); // => xaxisTitles = [[1, "title1"], [2, "title2"], [3, "title3"]]
+      // => parsedData  = [[1, 100], [2, 200], [3, 300]]
 
-  // for line in data
-  for(var i=1;i<data.length;i++){
-    var line = data[i];
-    // for item in line
-    for(var n=0;n<line.length;n++){
-      intValue = parseInt(line[n]);
-      parsedData.push([n+1,intValue]);
-    }
-  }
-  // console.log(JSON.stringify(parsedData));
   callback(xaxisTitles, parsedData);
 }
 
